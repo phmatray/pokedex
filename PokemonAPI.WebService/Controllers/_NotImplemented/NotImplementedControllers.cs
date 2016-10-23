f839c95578c813c359387d4a482475d2a614a3b6
Philippe Matray <phmatray@outlook.com>
2016-10-23 09:57:33 +0200

    Add SuperContestEffectsController

--- PokemonAPI.WebService/Controllers/_NotImplemented/NotImplementedControllers.cs
+++ PokemonAPI.WebService/Controllers/_NotImplemented/NotImplementedControllers.cs
@@ -113,32 +113,4 @@ namespace PokemonAPI.WebService.Controllers
             throw new NotImplementedException();
         }
     }
-
-    [Route("api/v1/super-contest-effects")]
-    public class SuperContestEffectsController : ApiController
-    {
-        private readonly VeekunContext _context;
-
-        public SuperContestEffectsController(VeekunContext context)
-        {
-            _context = context;
-        }
-
-        // GET api/v1/super-contest-effects
-        // GET api/v1/super-contest-effects?skip=0&take=20
-        [HttpGet]
-        public async Task<IActionResult> GetAll(int limit = 20, int offset = 0)
-        {
-            throw new NotImplementedException();
-            //return await base.GetAll(limit, offset,
-            //    _context.Characteristics, this.Segment());
-        }
-
-        // GET api/v1/super-contest-effects/1
-        [HttpGet("{id}")]
-        public async Task<IActionResult> Get(int id)
-        {
-            throw new NotImplementedException();
-        }
-    }
 }
---
+++ PokemonAPI.WebService/SuperContestEffectsController.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using PokemonAPI.Models.Rsc;
+using PokemonAPI.WebService.Controllers.Base;
+using PokemonAPI.WebService.Core;
+using PokemonAPI.WebService.Models;
+
+namespace PokemonAPI.WebService.Controllers
+{
+    [Route("api/v1/super-contest-effects")]
+    public class SuperContestEffectsController : ApiController
+    {
+        private readonly VeekunContext _context;
+
+        public SuperContestEffectsController(VeekunContext context)
+        {
+            _context = context;
+        }
+
+        // GET api/v1/super-contest-effects
+        // GET api/v1/super-contest-effects?skip=0&take=20
+        [HttpGet]
+        public async Task<IActionResult> GetAll(int limit = 20, int offset = 0)
+        {
+            try
+            {
+                if (limit <= 0) throw new ArgumentOutOfRangeException(nameof(limit));
+                if (offset < 0) throw new ArgumentOutOfRangeException(nameof(offset));
+
+                var dbset = _context.SuperContestEffects;
+                var urlSegment = typeof(ContestEffectsController).Segment();
+
+                var count = await dbset.CountAsync();
+                var previous = Previous(limit, offset, urlSegment);
+                var next = Next(limit, offset, count, urlSegment);
+
+                var apiResults = (await dbset
+                        .OrderBy(x => x.Id)
+                        .Skip(offset)
+                        .Take(limit)
+                        .ToListAsync())
+                    .Select(x => x.ToApiResource(urlSegment))
+                    .ToList();
+
+                var results = new APIResourceList(count, previous, next, apiResults);
+
+                return Ok(results);
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex);
+            }
+        }
+
+        // GET api/v1/super-contest-effects/1
+        [HttpGet("{id}")]
+        public async Task<IActionResult> Get(int id)
+        {
+            try
+            {
+                var superContestEffect = await _context.SuperContestEffects
+                    .Include(x => x.SuperContestEffectProse).ThenInclude(x => x.LocalLanguage)
+                    .Include(x => x.Moves)
+                    .FirstOrDefaultAsync(x => x.Id == id);
+
+                var result = new SuperContestEffect
+                {
+                    Id                = superContestEffect.Id,
+                    Appeal            = superContestEffect.Appeal,
+                    FlavorTextEntries = GetFlavorTextEntries(superContestEffect),
+                    Moves             = GetMoves(superContestEffect)
+                };
+
+                return Ok(result);
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex);
+            }
+        }
+
+        private List<FlavorText> GetFlavorTextEntries(EFSuperContestEffects superContestEffect)
+        {
+            return superContestEffect
+                .SuperContestEffectProse
+                .Select(x => new FlavorText
+                (
+                    x.FlavorText,
+                    x.LocalLanguage.ToNamedApiResource<LanguagesController>()
+                ))
+                .ToList();
+        }
+
+        private List<NamedAPIResource> GetMoves(EFSuperContestEffects superContestEffect)
+        {
+            return superContestEffect
+                .Moves
+                .Select(x => x.ToNamedApiResource<MovesController>())
+                .ToList();
+        }
+    }
+}
