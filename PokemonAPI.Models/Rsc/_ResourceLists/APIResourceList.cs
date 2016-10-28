1b997ca97594c10e371ff5f3f6d00234e2b45b92
Philippe Matray <phmatray@outlook.com>
2016-10-28 12:14:40 +0200

    Make Resources list generic

--- PokemonAPI.Models/Rsc/_ResourceLists/APIResourceList.cs
+++ PokemonAPI.Models/Rsc/_ResourceLists/APIResourceList.cs
@@ -2,35 +2,11 @@
 
 namespace PokemonAPI.Models.Rsc
 {
-    public class APIResourceList
+    public class APIResourceList : ResourceList<APIResource>
     {
         public APIResourceList(int count, string previous, string next, List<APIResource> results)
+            : base(count, previous, next, results)
         {
-            Count    = count;
-            Previous = previous;
-            Next     = next;
-            Results  = results;
         }
-
-        /// <summary>
-        /// The total number of resources available from this API
-        /// </summary>
-        public int Count { get; set; }
-
-        /// <summary>
-        /// The URL for the next page in the list
-        /// </summary>
-        public string Next { get; set; }
-
-        /// <summary>
-        /// The URL for the previous page in the list
-        /// </summary>
-        public string Previous { get; set; }
-
-        /// <summary>
-        /// A list of unnamed API resources
-        /// </summary>
-        public List<APIResource> Results { get; set; }
-
     }
 }
--- PokemonAPI.Models/Rsc/_ResourceLists/NamedAPIResourceList.cs
+++ PokemonAPI.Models/Rsc/_ResourceLists/NamedAPIResourceList.cs
@@ -2,35 +2,11 @@ using System.Collections.Generic;
 
 namespace PokemonAPI.Models.Rsc
 {
-    public class NamedAPIResourceList
+    public class NamedAPIResourceList : ResourceList<NamedAPIResource>
     {
         public NamedAPIResourceList(int count, string previous, string next, List<NamedAPIResource> results)
+            : base(count, previous, next, results)
         {
-            Count    = count;
-            Previous = previous;
-            Next     = next;
-            Results  = results;
         }
-
-        /// <summary>
-        /// The total number of resources available from this API
-        /// </summary>
-        public int Count { get; set; }
-
-        /// <summary>
-        /// The URL for the next page in the list
-        /// </summary>
-        public string Next { get; set; }
-
-        /// <summary>
-        /// The URL for the previous page in the list
-        /// </summary>
-        public string Previous { get; set; }
-
-        /// <summary>
-        /// A list of named API resources
-        /// </summary>
-        public List<NamedAPIResource> Results { get; set; }
-
     }
 }
